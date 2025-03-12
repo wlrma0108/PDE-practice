326d3e4f0ec42eb9f1705232d26f02eee09e5bca
kim hojoong <82634705+wlrma0108@users.noreply.github.com>
2025-03-12 11:08:32 +0900

    1-8

---
+++ overflowdouble.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<climits>
+//LLONG_MAX 가 포함된 라이브러리
+
+int main(){
+    long long no_overflow = LLONG_MAX;
+    long long overflow = LLONG_MAX;
+    //long long으로 8바이트 단위를 얻는다.
+    // 위의 문제와 굉장히 유사하다. 
+    overflow += LLONG_MAX;
+    std:: cout << overflow;
+    std:: cout << no_overflow;
+
+}
